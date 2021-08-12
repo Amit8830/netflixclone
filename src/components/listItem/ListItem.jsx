23fb706d6d98280c18a3cfd236ff1d225cc21dac
Amit8830 <gamit9022@gmail.com>
2021-08-12 23:17:50 +0530

    initial commit

---
+++ src/components/listItem/ListItem.jsx
@@ -0,0 +1,53 @@
+import "./listItem.scss";
+import {
+  PlayArrow,
+  Add,
+  ThumbUpAltOutlined,
+  ThumbDownOutlined,
+} from "@material-ui/icons";
+import { useState } from "react";
+
+export default function ListItem({ index }) {
+  const [isHovered, setIsHovered] = useState(false);
+  const trailer =
+    "https://www.istockphoto.com/video/sea-clouds-from-the-top-of-the-mountain-gm1288024060-384102118?utm_source=pixabay&utm_medium=affiliate&utm_campaign=SRP_video_sponsored&referrer_url=http%3A%2F%2Fpixabay.com%2Fvideos%2Fsearch%2Fnetflix%2F&utm_term=netflix";
+  return (
+    <div
+      className="listItem"
+      style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <img
+        src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee"
+        alt=""
+      />
+      
+      {isHovered && (
+        <>
+          <video src={trailer} autoPlay={true} loop />
+          <div className="itemInfo">
+            <div className="icons">
+              <PlayArrow className="icon" />
+              <Add className="icon" />
+              <ThumbUpAltOutlined className="icon" />
+              <ThumbDownOutlined className="icon" />
+            </div>
+            <div className="itemInfoTop">
+              <span>2 hour 14 mins</span>
+              <span className="limit">+16</span>
+              <span>1999</span>
+            </div>
+            <div className="desc">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Praesentium hic rem eveniet error possimus, neque ex doloribus.
+            </div>
+            <div className="genre">Action</div>
+          </div>
+        </>
+      )}
+    </div>
+
+    
+  );
+}
